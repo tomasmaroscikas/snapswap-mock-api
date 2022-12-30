49c8d3f791165d0dfdb13cccc17f97d46c99b659
tomasmaroscikas <tomas.maroscikas@gmail.com>
2022-12-30 20:54:27 +0200

    sec: improved script to avoid specifying cacer path explicitly

--- src/test/shell/addSelfSignedCert.sh
+++ src/test/shell/addSelfSignedCert.sh
@@ -1,3 +1,9 @@
+#!/bin/bash
+
 echo -n | openssl s_client -connect localhost:8443 | sed -ne '/-BEGIN CERTIFICATE-/,/-END CERTIFICATE-/p' > micronautcert.crt
-keytool -delete -noprompt -alias "micronaut" -keystore /usr/local/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home/lib/security/cacerts -storepass changeit
-keytool -import -trustcacerts -keystore /usr/local/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home/lib/security/cacerts -storepass changeit -noprompt -alias micronaut -file micronautcert.crt
+
+#cacert_store=/usr/local/Cellar/openjdk@11/11.0.15/libexec/openjdk.jdk/Contents/Home/lib/security/cacerts
+#cacert_store=/home/tomas/.sdkman/candidates/java/11.0.12-open/lib/security/cacerts
+
+keytool -delete -noprompt -alias "micronaut" -trustcacerts -cacerts -storepass changeit
+keytool -import -trustcacerts -cacerts -storepass changeit -noprompt -alias micronaut -file micronautcert.crt
