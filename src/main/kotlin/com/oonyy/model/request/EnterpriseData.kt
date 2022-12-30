d9ff1238a6982a502123926543eabbe2aa103554
Tomas Maroščikas <tomas.maroscikas@gmail.com>
2022-12-30 17:56:10 +0200

    fix: field name in enterprise request

--- src/main/kotlin/com/oonyy/model/request/EnterpriseData.kt
+++ src/main/kotlin/com/oonyy/model/request/EnterpriseData.kt
@@ -1,10 +1,11 @@
 package com.oonyy.model.request
 
 import com.fasterxml.jackson.annotation.JsonProperty
+import io.micronaut.core.annotation.Introspected
 
-data class EnterpriseData(@param:JsonProperty("companyName") val companyName: String,
-                          @param:JsonProperty("registrationNumber") val registrationNumber: String,
-                          @param:JsonProperty("registrationCountry") val registrationCountry: String,
-                          @param:JsonProperty("registrationAddress") val registrationAddress: String,
-                          @param:JsonProperty("businessAddress") val businessAddress: String,
+data class EnterpriseData(@param:JsonProperty("company_name") val companyName: String,
+                          @param:JsonProperty("registration_number") val registrationNumber: String,
+                          @param:JsonProperty("country") val registrationCountry: String,
+                          @param:JsonProperty("registration_address") val registrationAddress: String,
+                          @param:JsonProperty("business_address") val businessAddress: String,
 )
